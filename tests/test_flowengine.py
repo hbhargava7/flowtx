54fc2e7d395663d97f89e0ac6e1864a0b725a476
Hersh Bhargava <hershkbhargava@gmail.com>
2023-09-03 20:54:48 -0700

    Update test assertions
    Signed-off-by: Hersh Bhargava <hershkbhargava@gmail.com>

--- tests/test_flowengine.py
+++ tests/test_flowengine.py
@@ -4,8 +4,8 @@ from flowtx.flowengine import FlowEngine
 
 def test_flowengine_init_from_file():
     engine = FlowEngine(wsp_path='tests/data/test_workspace.wsp', use_cache=False)
-    assert len(engine.wsp.get_sample_ids()) == 10
+    assert len(engine.wsp.get_sample_ids()) == 5
 
 def test_flowengine_init_from_cache():
     engine = FlowEngine(use_cache=True, cache_path='tests/data/wsp_cache.pkl')
-    assert len(engine.wsp.get_sample_ids()) == 10
+    assert len(engine.wsp.get_sample_ids()) == 5
