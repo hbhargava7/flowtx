8791101eea7a9843369d0534ebca4b2254f05491
Hersh Bhargava <hershkbhargava@gmail.com>
2023-09-03 20:53:47 -0700

    Reduce size of static test data
    Signed-off-by: Hersh Bhargava <hershkbhargava@gmail.com>

--- tests/test_flowengine.py
+++ tests/test_flowengine.py
@@ -2,6 +2,10 @@ import pytest
 
 from flowtx.flowengine import FlowEngine
 
-def test_flowengine_init_fromfile():
+def test_flowengine_init_from_file():
     engine = FlowEngine(wsp_path='tests/data/test_workspace.wsp', use_cache=False)
+    assert len(engine.wsp.get_sample_ids()) == 10
+
+def test_flowengine_init_from_cache():
+    engine = FlowEngine(use_cache=True, cache_path='tests/data/wsp_cache.pkl')
     assert len(engine.wsp.get_sample_ids()) == 10
